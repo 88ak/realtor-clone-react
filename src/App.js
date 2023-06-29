14ecb4108921e9cba11ea862cc5a8f9840e5aa34
Aniket Kumbhar <anikettkum88@gmail.com>
2023-06-29 22:43:24 +0530

    Create the create listing page UI

--- src/App.js
+++ src/App.js
@@ -8,37 +8,39 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
     <>
-        <Router>
-          <Header/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<PrivateRoute/>}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
-            </Route>
-            
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/offers" element={<Offers />} />
-          </Routes>
-        </Router>
-        <ToastContainer
-position="bottom-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/> 
+          </Route>
+
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,75 +1,111 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { doc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
 export default function Profile() {
   const auth = getAuth();
   const Navigate = useNavigate();
-  const [changeDetail, setChangeDetail] = useState(false)
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-  const {name, email} = formData;
-  function onLogout(){
+  const { name, email } = formData;
+  function onLogout() {
     auth.signOut();
     Navigate("/");
-
   }
-  function onChange(e){
-    setFormData((prevState)=>({
+  function onChange(e) {
+    setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
+    }));
   }
-  async function onSubmit(){
+  async function onSubmit() {
     try {
-      if(auth.currentUser.displayName !== name){
-      //update details in firebase authentication 
-      await updateProfile(auth.currentUser,{
-        displayName: name,
-      });
-      //update name in firestore
-      const docRef = doc(db, "users", auth.currentUser.uid)
-      await updateDoc(docRef, {
-        name,
-      });
-    }
-    toast.success('profile details updated')
+      if (auth.currentUser.displayName !== name) {
+        //update details in firebase authentication
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //update name in firestore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("profile details updated");
     } catch (error) {
-      toast.error("could not update the profile details")
+      toast.error("could not update the profile details");
     }
   }
   return (
     <>
-    <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
-      <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
-      <div className='w-full md:w-[50%] mt-6 px-3'>
-        <form>
-          {/*Name Input*/}
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/*Name Input*/}
 
-          <input type="text" id="name" value={name} disabled={!changeDetail}
-          onChange={onChange}
-           className={`mb-6 w-full px-4 py-2 text-xl text-gary-700 bg-white border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200" }` }/>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gary-700 bg-white border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
 
-          {/*Email Input*/}
+            {/*Email Input*/}
 
-          <input type="email" id="email" value={email} disabled
-           className='w-full px-4 py-2 text-xl text-gary-700 bg-white border-gray-300 rounded transition ease-in-out mb-6 '/>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gary-700 bg-white border-gray-300 rounded transition ease-in-out mb-6 "
+            />
 
-          <div className='flex justify-between whitespace-nowrap'>
-            <p>Do you want to change your Name? <span onClick={()=>{
-              changeDetail && onSubmit()
-              setChangeDetail((prevState)=>!prevState)
-            }} className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>{changeDetail ? "Apply Change" : "Edit"}</span></p>
-            <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transtion duration-200 ease-in-out cursor-pointer'>Sign-out</p>
-          </div>
-        </form>
-      </div>
-    </section>
-    
+            <div className="flex justify-between whitespace-nowrap">
+              <p>
+                Do you want to change your Name?{" "}
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply Change" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transtion duration-200 ease-in-out cursor-pointer"
+              >
+                Sign-out
+              </p>
+            </div>
+          </form>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-small font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 mt-4"
+          >
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center"
+            >
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or Rent your Home
+            </Link>
+          </button>
+        </div>
+      </section>
     </>
-  )
+  );
 }
